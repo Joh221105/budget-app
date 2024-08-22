b7c3a8a5badd33bd8c57cf8e68b2da71947a8599
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-08-22 13:21:23 -0700

    adds Mongodb connect code to app.js

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,14 @@ dotenv.config();
 app.use(cors()); 
 app.use(express.json()); 
 
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
+
 app.get('/', (req, res) => {
     res.send('Placeholder');
 });
